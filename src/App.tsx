cc292806741a43a03d00f3f912446ad5c02c6c94
Farzaneh <farzanekazemi8517@gmail.com>
2023-08-21 18:54:31 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className='body_wrapper'>
+      <div className="form_wrapper">
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
