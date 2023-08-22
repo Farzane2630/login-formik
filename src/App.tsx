2b1ceb2c1e1788a6e3c4eb625b107d009f41ed7e
Farzaneh <farzanekazemi8517@gmail.com>
2023-08-22 17:56:43 +0330

    version 1.0 (before Yup)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,85 @@
+import { Field, Form, Formik } from 'formik'
 import './App.scss'
+
+interface formValues {
+  name: string
+  email: string
+  password: string
+}
 
 function App() {
 
   return (
     <div className='body_wrapper'>
       <div className="form_wrapper">
-        
+        <div className="form">
+          <h1 className="title">
+            Create an account
+          </h1>
+          <p className="subtitle">
+            Sign up now and unlock exclusive access!
+          </p>
+          <Formik
+            initialValues={{ email: '', name: '', password: "" }}
+            onSubmit={(values: formValues, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+            validate={values => {
+              const errors: { name?: string, email?: string, password?: string } = {}
+
+              if (!values.name) {
+                errors.name = "Required"
+              } else if (values.name.length < 3 || values.name.length > 15) {
+                errors.name = "name must be more than 3 and less than 15 letters"
+              }
+              if (!values.password) {
+                errors.password = "Required"
+              } else if (values.password.length < 8 || values.password.length > 15) {
+                errors.password = "password must be more than 8 and less than 15 letters"
+              }
+
+              if (!values.email) {
+                errors.email = "Required"
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = 'Invalid email address';
+              }
+
+              return errors;
+            }}
+          >
+            {({ errors, touched, isSubmitting }) => (
+              <Form className='form_parent'>
+                <label className='lable' htmlFor="name">Your name</label>
+                {errors.name && touched.name && errors.name}
+                <Field type="text" name='name' placeholder='First Last' className='name' />
+                <label className='lable' htmlFor="email">Email</label>
+                {errors.email && touched.email && errors.email}
+                <Field type="text" name='email' placeholder='you@email.com' className='email' />
+                <label className='lable' htmlFor="password">Password</label>
+                {errors.password && touched.password && errors.password}
+                <Field type="password" name='password' placeholder='⚫⚫⚫⚫⚫⚫⚫⚫' className='password' />
+
+                <button className="create_account" type='submit' disabled={isSubmitting}>
+                  Create Account
+                </button>
+                <button type='button' className="log_in">
+                  Log in
+                </button>
+              </Form>
+            )
+            }
+          </Formik>
+        </div>
+        <div className="img_wrapper">
+          <img src="src/assets/pic.png" className='img' alt="img" />
+        </div>
       </div>
-      
+
     </div>
   )
 }
